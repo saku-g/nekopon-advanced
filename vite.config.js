63d5b1ae528eda4dd63f10b43ab51f00e572e0c1
saku-g <sakucode1993@gmail.com>
2024-01-25 22:49:25 +0900

    First commit

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import sassGlobImports from 'vite-plugin-sass-glob-import';
+import vituum from 'vituum';
+import pug from '@vituum/vite-plugin-pug';
+
+// https://ja.vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: true, // IPアドレス有効化
+    open: true,
+    port: 8080,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: true,
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (/\.css$/.test(assetInfo.name)) {
+            return 'assets/css/[name].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        },
+      },
+    },
+  },
+  plugins: [
+    // https://vituum.dev/
+    vituum(),
+    pug({
+      root: './src',
+    }),
+    sassGlobImports(),
+  ],
+});
