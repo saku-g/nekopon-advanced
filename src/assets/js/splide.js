003d970566bc07a6f37964c08991be76b4c1f392
saku-g <sakucode1993@gmail.com>
2024-02-04 20:41:04 +0900

    Add index works style

--- src/assets/js/splide.js
+++ src/assets/js/splide.js
@@ -1,4 +1,15 @@
 import '@splidejs/splide/css';
 import Splide from '@splidejs/splide';
 
-new Splide('.splide').mount();
+const worksSlider = new Splide('#js-worksSlider', {
+  // https://splidejs.com/guides/options/
+  type: 'fade',
+  rewind: true,
+  classes: {
+    arrows: 'splide__arrows your-class-arrows',
+    arrow: 'splide__arrow your-class-arrow',
+    prev: 'splide__arrow--prev your-class-prev',
+    next: 'splide__arrow--next your-class-next',
+  },
+});
+worksSlider.mount();
