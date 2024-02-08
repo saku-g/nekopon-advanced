e92a77236baff6935776ecd4f12df5c2871afaae
saku-g <sakucode1993@gmail.com>
2024-02-08 22:54:39 +0900

    Add skipLink styles

--- src/assets/js/drawer.js
+++ src/assets/js/drawer.js
@@ -2,7 +2,6 @@ const button = document.getElementById('js-menuButton');
 
 button.addEventListener('click', (e) => {
   const isExpanded = e.currentTarget.getAttribute('aria-expanded') !== 'true';
-  console.log(isExpanded);
   e.currentTarget.setAttribute('aria-expanded', isExpanded);
 
   document.documentElement.classList.toggle('is-drawerActive');
